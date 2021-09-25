1452709b0bc65dc9b1458e59e02b44facd80b1c3
AhmedIbrahimAbdellatif <Ahmed.Ahmed99@eng-st.cu.edu.eg>
2021-09-25 23:23:40 +0200

    update quality function per type

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,57 +16,69 @@ class GildedRose {
     private void updateQualityForItem(Item item) {
         switch (item.name) {
             case "Aged Brie":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+                agedBrieUpdateQuality(item);
                 break;
             case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
+                backstageUpdatedQuality(item);
                 break;
             case "Sulfuras, Hand of Ragnaros":
 
                 break;
             default:
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                defaultUpdatedQuality(item);
+        }
+    }
 
-                item.sellIn = item.sellIn - 1;
+    private void defaultUpdatedQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
 
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
+        return;
+    }
+
+    private void backstageUpdatedQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
-                break;
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void agedBrieUpdateQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
         }
     }
 
